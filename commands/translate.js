9153a442228fd05b6f8facae6db135a920c5830f
Lucas Draescher <draescherl@eisti.eu>
2020-07-28 12:50:51 +0200

    Removed unnecessary try/catch block

--- commands/translate.js
+++ commands/translate.js
@@ -10,35 +10,34 @@ module.exports = {
         const file = './data.json';
 
         var words;
-        try {
-            // Get message just before command was run :
-            channel.messages.fetch({ limit: 2 }).then(messages => {
-                let lastMessage = messages.last().content.toLowerCase();
-                let author = messages.last().author.toString();
-                
-                // Get data from data.json :
-                data = JSON.parse(fs.readFileSync(file));
-                words = data.words;
-                let correctedMessage = lastMessage;
+        
+        // Get message just before command was run :
+        channel.messages.fetch({ limit: 2 }).then(messages => {
+            let lastMessage = messages.last().content.toLowerCase();
+            let author = messages.last().author.toString();
+            
+            // Get data from data.json :
+            data = JSON.parse(fs.readFileSync(file));
+            words = data.words;
+            let correctedMessage = lastMessage;
 
-                // Compare message to data :
-                for (const word of words) {
-                    let expression = word.expression.toLowerCase();
-                    let translation = word.translation.toLowerCase();
+            // Compare message to data :
+            for (const word of words) {
+                let expression = word.expression.toLowerCase();
+                let translation = word.translation.toLowerCase();
 
-                    // Replace if necessary :
-                    if (typeof lastMessage == 'string' && lastMessage.indexOf(expression) > -1) {
-                        correctedMessage = lastMessage.replace(expression, translation);
-                    }
+                // Replace if necessary :
+                if (typeof lastMessage == 'string' && lastMessage.indexOf(expression) > -1) {
+                    correctedMessage = lastMessage.replace(expression, translation);
                 }
-                
-                // Send message in channel :
-                channel.send(`${author} voulais dire : ${correctedMessage}`);
-            });
-        } catch (err) {
-            console.log('Error message :');
-            console.log(err);
+            }
+            
+            // Send message in channel :
+            channel.send(`${author} voulais dire : ${correctedMessage}`);
+        }).catch((error) => {
+            console.error(error);
             channel.send('Fichier de données non trouvé. Essayez d\'exécuter \`-update\` et réessayez.');
-        }
+        });
+        
     }
 }
